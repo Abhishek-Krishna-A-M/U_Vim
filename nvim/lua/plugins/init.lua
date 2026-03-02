a83c9e083242c2c8d3b8ba9527c7a38d49f4fb8b
Abhishek Krishna A M <abhishekkrishna2k6@gmail.com>
2026-03-02 23:53:05 +0530

    Delete nvim directory

--- nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require('plugins.gitsigns'),
-  require('plugins.telescope'),
-  require('plugins.lsp'),
-  require('plugins.completion'),
-  require('plugins.formatter'),
-  require('plugins.treesitter'),
-}
-
